e163b104d61e32faea212ba182d0839eff9dd5e3
watabee <watabee.dev@gmail.com>
2020-06-12 23:00:34 +0900

    Add android studio preview beta & canary

---
+++ android-studio-preview-canary.rb
@@ -0,0 +1,27 @@
+cask 'android-studio-preview-canary' do
+  version '4.2.0.1,201.6568795'
+  sha256 'c435f9d93c729a26bdedb3c58ff48482b2aaf43e65976e7470c82b2dd19b6bec'
+
+  # dl.google.com/dl/android/studio/ was verified as official when first introduced to the cask
+  url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
+  name 'Android Studio Preview (Canary)'
+  homepage 'https://developer.android.com/studio/preview/'
+
+  app "Android Studio #{version.major_minor} Preview.app"
+
+  zap trash: [
+               '~/Library/Android/sdk',
+               "~/Library/Application Support/AndroidStudio#{version.major_minor}",
+               "~/Library/Caches/AndroidStudio#{version.major_minor}",
+               "~/Library/Logs/AndroidStudio#{version.major_minor}",
+               "~/Library/Preferences/AndroidStudio#{version.major_minor}",
+               '~/Library/Preferences/com.android.Emulator.plist',
+               '~/Library/Saved Application State/com.google.android.studio.savedState',
+               '~/.android',
+             ],
+      rmdir: [
+               '~/AndroidStudioProjects',
+               '~/Library/Android',
+             ]
+end
+
